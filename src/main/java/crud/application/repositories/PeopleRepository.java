16be03bac0a1ec9520d9f62bfe2016a86ef79d4e
”Dmitry <pakhomov.arz@gmail.com>
2022-07-01 23:36:58 +0300

    Crud app

---
+++ src/main/java/crud/application/repositories/PeopleRepository.java
@@ -0,0 +1,10 @@
+package crud.application.repositories;
+
+import crud.application.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+}
